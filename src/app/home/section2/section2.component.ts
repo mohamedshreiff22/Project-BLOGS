b6c68cda7ff57d82319515e942ef74410ab1cec8
Mohamed-Sherif <mohamedshreiff@gmail.com>
2024-10-09 16:53:12 +0200

    update blog card

--- src/app/home/section2/section2.component.ts
+++ src/app/home/section2/section2.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section2',
   standalone: true,
-  imports: [],
+  imports: [Section2Component],
   templateUrl: './section2.component.html',
   styleUrl: './section2.component.css'
 })
