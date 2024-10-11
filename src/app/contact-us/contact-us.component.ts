8eb8d0d66469c501a753ef8ae9e4cc3410fc1ae5
YoussefShafik1 <150313863+YoussefShafik1@users.noreply.github.com>
2024-10-12 02:10:34 +0300

    I fixed the background img of contact us

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Renderer2,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrl: './contact-us.component.css',
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit, OnDestroy {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object // Inject PLATFORM_ID to check if it's browser
+  ) {}
 
+  ngOnInit() {
+    // Check if the platform is a browser before accessing the document
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.addClass(document.body, 'contact-background');
+    }
+  }
+
+  ngOnDestroy() {
+    // Check if the platform is a browser before accessing the document
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.removeClass(document.body, 'contact-background');
+    }
+  }
 }
