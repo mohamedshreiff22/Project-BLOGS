51e70978a06d5b1683caa8de1fa41e4074ef81c7
Mohamed-Sherif <mohamedshreiff@gmail.com>
2024-10-12 18:09:09 +0200

    blogdetalis

---
+++ src/app/Blog-details/Blog-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Blog-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './Blog-details.component.html',
+  styleUrl: './Blog-details.component.css'
+})
+export class BlogDetailsComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { FooterComponent } from './footer/footer.component';
-import { BlogsComponent } from './blogs/blogs.component';
+import { BlogDetailsComponent } from './Blog-details/Blog-details.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { MoreDestinationsComponent } from './more-destinations/more-destinations.component';
-import { routes } from './app.routes';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,7 +19,7 @@ import { routes } from './app.routes';
     LoginFormComponent,
     SignUpComponent,
     FooterComponent,
-    BlogsComponent,
+    BlogDetailsComponent,
     ContactUsComponent,
     PageNotFoundComponent,
     HomeComponent,
