0a757b81c32ea2246856e04674cf4f39e54f17c7
Dr-Abdulrahman-Essam <ae1198@fayoum.edu.eg>
2024-10-16 19:10:11 +0300

    Add files via upload

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Add CommonModule here
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
+  teamMembers = [
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    {
+      name: 'Dr. Eng. Abdulrahman Essam EL-Agry',
+      title: 'Project Manager',
+      photo: 'assets/abdo.png',
+      bio: 'Civil Engineering Consultant, HSE certified, Committee Member of MLD Governorate',
+    },
+    // Add more team members as needed
+  ];
 
+  selectedMember: any = null;
+
+  openBio(member: any) {
+    this.selectedMember = member;
+    document.body.classList.add('modal-open');
+  }
+
+  closeBio() {
+    this.selectedMember = null;
+    document.body.classList.remove('modal-open');
+  }
 }
