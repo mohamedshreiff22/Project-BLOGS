b5823cb8ddf5d7afe5ea643f7bad6ae8cfaebb84
Mohamed-Sherif <mohamedshreiff@gmail.com>
2024-10-19 16:19:40 +0200

    edit footer

--- src/app/home/tags/one-component/one-component.component.ts
+++ src/app/home/tags/one-component/one-component.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common'; // إضافة هذا السطر
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2'; // استيراد SweetAlert2
 
 @Component({
   selector: 'app-one',
   standalone: true,
   templateUrl: './one-component.component.html',
   styleUrls: ['./one-component.component.css'],
-  imports: [CommonModule] // إضافة هذا السطر
+  imports: [CommonModule]
 })
 export class OneComponent implements OnInit {
   tag: string = '';
@@ -36,6 +37,52 @@ export class OneComponent implements OnInit {
 
     this.comments.push(newComment);
     localStorage.setItem('comments', JSON.stringify(this.comments));
+
+    // عرض رسالة تأكيد
+    Swal.fire({
+      icon: 'success',
+      title: 'Comment Added!',
+      text: 'Your comment has been successfully added.',
+    });
+  }
+
+  editComment(index: number) {
+    const commentToEdit = this.comments[index];
+
+    Swal.fire({
+      title: 'Edit Comment',
+      input: 'textarea',
+      inputValue: commentToEdit.text,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (result.value) {
+          commentToEdit.text = result.value; // تحديث نص التعليق
+          localStorage.setItem('comments', JSON.stringify(this.comments)); // حفظ التغييرات
+          Swal.fire('Updated!', 'Your comment has been updated.', 'success');
+        }
+      }
+    });
+  }
+
+  deleteComment(index: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.comments.splice(index, 1); // حذف التعليق
+        localStorage.setItem('comments', JSON.stringify(this.comments)); // حفظ التغييرات
+        Swal.fire('Deleted!', 'Your comment has been deleted.', 'success');
+      }
+    });
   }
 
   getImageUrl() {
