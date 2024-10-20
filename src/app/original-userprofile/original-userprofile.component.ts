379ea576d5ed3cfa6f8f7ceadb41f46d98a6c5ac
YoussefShafik1 <150313863+YoussefShafik1@users.noreply.github.com>
2024-10-20 17:18:35 +0300

    user profile component added

---
+++ src/app/original-userprofile/original-userprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-original-userprofile',
+  standalone: true,
+  imports: [],
+  templateUrl: './original-userprofile.component.html',
+  styleUrl: './original-userprofile.component.css'
+})
+export class OriginalUserprofileComponent {
+
+}
